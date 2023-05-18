d232c750f2973f787ef2525803b033a41d006571
Joe Dixon <hello@joedixon.co.uk>
2023-05-18 10:33:18 +0100

    add tests

--- tests/Unit/FileProviderTest.php
+++ tests/Unit/FileProviderTest.php
@@ -2,9 +2,7 @@
 
 namespace JoeDixon\Translation\Tests;
 
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\File;
-use JoeDixon\Translation\Events\TranslationAdded;
 use JoeDixon\TranslationCore\Exceptions\LanguageExistsException;
 use JoeDixon\TranslationCore\Translation;
 use JoeDixon\TranslationCore\Translations;
@@ -18,7 +16,7 @@ beforeEach(function () {
     $this->translation = $this->app->make(Translation::class);
 });
 
-afterAll(function () {
+afterEach(function () {
     File::deleteDirectory($this->app->langPath());
 });
 
@@ -207,97 +205,16 @@ it('can return a full list of available keys across all languages', function () 
         ));
 });
 
-// test('a_list_of_languages_can_be_viewed', function () {
-//     $this->get(config('translation.ui_url'))
-//         ->assertSee('en');
-// });
+it('can save a string key translation in an empty file if it exists', function () {
+    $this->translation->addStringKeyTranslation('jp', 'Hello', 'こんにちは');
 
-// test('the_language_creation_page_can_be_viewed', function () {
-//     $this->get(config('translation.ui_url').'/create')
-//         ->assertSee('Add a new language');
-// });
+    expect($this->translation->allTranslationsFor('jp')->string()->toArray())
+        ->toEqual(['Hello' => 'こんにちは']);
+});
 
-// test('a_language_can_be_added', function () {
-//     $this->post(config('translation.ui_url'), ['locale' => 'de'])
-//         ->assertRedirect();
+it('can add a string key translation to an existing file', function () {
+    $this->translation->addStringKeyTranslation('en', 'Hey', 'Hey there!');
 
-//     $this->assertTrue(file_exists(__DIR__.'/fixtures/lang/de.json'));
-//     $this->assertTrue(file_exists(__DIR__.'/fixtures/lang/de'));
-
-//     rmdir(__DIR__.'/fixtures/lang/de');
-//     unlink(__DIR__.'/fixtures/lang/de.json');
-// });
-
-// test('a_list_of_translations_can_be_viewed', function () {
-//     $this->get(config('translation.ui_url').'/en/translations')
-//         ->assertSee('hello')
-//         ->assertSee('whats_up');
-// });
-
-// test('the_translation_creation_page_can_be_viewed', function () {
-//     $this->get(config('translation.ui_url').'/'.config('app.locale').'/translations/create')
-//         ->assertSee('Add a translation');
-// });
-
-// test('a_new_translation_can_be_added', function () {
-//     $this->post(config('translation.ui_url').'/en/translations', ['key' => 'joe', 'value' => 'is cool'])
-//         ->assertRedirect();
-//     $translations = $this->translation->stringKeyTranslations('en');
-
-//     $this->assertEquals(['Hello' => 'Hello', 'What\'s up' => 'What\'s up!', 'joe' => 'is cool'], $translations->toArray()['string']);
-
-//     file_put_contents(
-//         app()['path.lang'].'/en.json',
-//         json_encode((object) ['Hello' => 'Hello', 'What\'s up' => 'What\'s up!'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
-//     );
-// });
-
-// test('a_translation_can_be_updated', function () {
-//     $this->post(config('translation.ui_url').'/en', ['group' => 'test', 'key' => 'hello', 'value' => 'Hello there!'])
-//         ->assertStatus(200);
-
-//     $translations = $this->translation->shortKeyTranslations('en');
-
-//     $this->assertEquals(['hello' => 'Hello there!', 'whats_up' => 'What\'s up!'], $translations->toArray()['test']);
-
-//     file_put_contents(
-//         app()['path.lang'].'/en/test.php',
-//         "<?php\n\nreturn ".var_export(['hello' => 'Hello', 'whats_up' => 'What\'s up!'], true).';'.\PHP_EOL
-//     );
-// });
-
-// test('adding_a_translation_fires_an_event_with_the_expected_data', function () {
-//     Event::fake();
-
-//     $data = ['key' => 'joe', 'value' => 'is cool'];
-//     $this->post(config('translation.ui_url').'/en/translations', $data);
-
-//     Event::assertDispatched(TranslationAdded::class, function ($event) use ($data) {
-//         return $event->language === 'en' &&
-//             $event->group === 'string' &&
-//             $event->value === $data['value'] &&
-//             $event->key === $data['key'];
-//     });
-//     file_put_contents(
-//         app()['path.lang'].'/en.json',
-//         json_encode((object) ['Hello' => 'Hello', 'What\'s up' => 'What\'s up!'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
-//     );
-// });
-
-// test('updating_a_translation_fires_an_event_with_the_expected_data', function () {
-//     Event::fake();
-
-//     $data = ['group' => 'test', 'key' => 'hello', 'value' => 'Hello there!'];
-//     $this->post(config('translation.ui_url').'/en/translations', $data);
-
-//     Event::assertDispatched(TranslationAdded::class, function ($event) use ($data) {
-//         return $event->language === 'en' &&
-//             $event->group === $data['group'] &&
-//             $event->value === $data['value'] &&
-//             $event->key === $data['key'];
-//     });
-//     file_put_contents(
-//         app()['path.lang'].'/en/test.php',
-//         "<?php\n\nreturn ".var_export(['hello' => 'Hello', 'whats_up' => 'What\'s up!'], true).';'.\PHP_EOL
-//     );
-// });
+    expect($this->translation->allTranslationsFor('en')->string()->toArray())
+        ->toEqual(['Hello' => 'Hello', "What's up" => "What's up!", 'Hey' => 'Hey there!', 'laravel-translation' => ['key' => 'value']]);
+});
