a12d8e3836604dc352910d0763ae1be3c32b03d3
Joe Dixon <hello@joedixon.co.uk>
2023-06-05 11:16:38 +0100

    fix tests

--- src/Providers/File/InteractsWithShortKeys.php
+++ src/Providers/File/InteractsWithShortKeys.php
@@ -66,7 +66,7 @@ trait InteractsWithShortKeys
         $key = array_shift($keys);
 
         $values = $translations->get($group);
-        $values[$key] = Arr::undot([implode('.', $keys) => $value]);
+        $values[$key] = empty($keys) ? $value : Arr::undot([implode('.', $keys) => $value]);
         $translations->put($group, collect($values));
 
         $this->saveShortKeyTranslations($language, $group, collect($translations->get($group)));
